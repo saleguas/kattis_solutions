2961b0dab0ed123453921c2d7ab4a7679fb9c475
Salvador Aleguas <salvadoraleguas@gmail.com>
2018-12-02 18:48:00 -0500

    all problems up to 1.3 difficulty completed

---
+++ _1.3/bela.cpp
@@ -0,0 +1,76 @@
+#define __USE_MINGW_ANSI_STDIO 0
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <sstream>
+#include <bitset>
+#include <math.h>
+#include <assert.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+template <class T>
+using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+template <class T>
+using ordered_multiset = __gnu_pbds::tree<T, __gnu_pbds::null_type, less_equal<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+#define PI atan2(0, -1)
+#define epsilon 0.000000001
+#define INF 5000000000000000000
+#define MOD 1000000007
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+int getVal(string s, char key)
+{
+  if(s[0] == 'A')
+    return 11;
+  if(s[0] == 'K')
+    return 4;
+  if(s[0] == 'Q')
+    return 3;
+  if(s[0] == 'J' && s[1] == key)
+    return 20;
+  if(s[0] == 'J')
+    return 2;
+  if(s[0] == 'T')
+    return 10;
+  if(s[0] == '9' && s[1] == key)
+    return 14;
+  return 0;
+}
+int main(){
+    //freopen("sort.in", "r", stdin); freopen("sort.out", "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(1);
+    int amt; cin >> amt;
+    char s; cin >> s;
+    int total = 0;
+    for(int x = 0; x < amt * 4; x++)
+    {
+      string yeet; cin >> yeet;
+      total += getVal(yeet, s);
+    }
+    cout << total;
+}
+
+/******************************
+yeah its shack wes and im getting really rich
+-mo bamba
+******************************/
---
+++ _1.3/bijele.cpp
@@ -0,0 +1,53 @@
+#define __USE_MINGW_ANSI_STDIO 0
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <sstream>
+#include <bitset>
+#include <math.h>
+#include <assert.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+template <class T>
+using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+template <class T>
+using ordered_multiset = __gnu_pbds::tree<T, __gnu_pbds::null_type, less_equal<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+#define PI atan2(0, -1)
+#define epsilon 0.000000001
+#define INF 5000000000000000000
+#define MOD 1000000007
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+int main(){
+    //freopen("sort.in", "r", stdin); freopen("sort.out", "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout << fixed << setprecision(1);
+    int king, queen, rook, bishop, knight, pawn;
+    cin >> king >> queen >> rook >> bishop >> knight >> pawn;
+    cout << (1 - king) << " " << (1 - queen) << " " << (2 - rook) << " " << (2 - bishop) << " " << (2 - knight) << " " << (8 - pawn);
+    
+}
+
+/******************************
+yeah its shack wes and im getting really rich
+-mo bamba
+******************************/
